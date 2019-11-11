f36d19b9f039cda5349198d81748922e98e5961d
CurtisFlaming <flamingc@ksu.edu>
2019-11-11 15:19:15 -0600

    page

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+var turn = "x"
+
+
+function setTurn() {
+    var turnElement = document.getElementById("turn")
+    turnElement.innerHTML = "It is player " + turn + "'s turn"
+}
+
+function onClick(event) {
+    event.prevntDefault()
+    if (!event.target.innerText) {
+        event.target.innerText = turn
+        if (turn === "x") {
+            turn = "o"
+        }
+        else {
+            turn = "x"
+        }
+        setTurn();
+    }
+}
+
+var cells = document.getElementsByClassName("square")
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", onClick)
+}
+
+setTurn()
